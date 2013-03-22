cdbec729cf721a6c60c22d1b30c35e5b44d83074
tracysanz <tracysanz@yahoo.com>
2013-03-21 20:13:15 -0400

    dev-sprint1

--- exercise54.py
+++ exercise54.py
@@ -1,2 +1,16 @@
 # This is where Exercise 5.4 goes
-# Name:
+# Name: Tamar Schanfeld
+def is_triangle(a,b,c):
+	if a > b + c or b > a + c or c > a + b:
+		print "No"
+	else:
+		print "Yes"
+
+
+def is_triangleinput():
+	a = int(raw_input('Enter first stick length: '))
+	b = int(raw_input('Enter second stick length: '))
+	c = int(raw_input('Enter third stick length: '))
+	is_triangle(a,b,c)
+
+is_triangleinput
